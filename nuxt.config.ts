babaf6a97e3c3a4e03501f8cc7d42bcb3b9b7e1b
auanauan <auanauan@users.noreply.github.com>
2025-08-20 17:03:56 +0700

    deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,11 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/image"],
+  nitro: {
+    prerender: {
+      routes: ['/']
+    }
+  },
   css: ["./app/css/global.css", "./app/css/fonts.css", "./app/css/utils.css"],
   app: {
     head: {
